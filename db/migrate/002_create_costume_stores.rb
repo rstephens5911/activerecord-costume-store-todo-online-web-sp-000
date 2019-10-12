7d8ff0b19f1a0b0fdeac53b4ffd58c8917decbef
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-12 22:55:00 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,7 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
 
   def change
-<<<<<<< HEAD
+
     create_table  :costume_stores do |t|
       t.string    :name
       t.string    :location
@@ -14,17 +14,4 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
   end
-=======
-    create_table :costume_stores do |t|
-      t.string    :name
-      t.string    :location
-      t.boolean   :open
-      t.integer   :costume_inventory
-      t.integer   :num_of_employees
-      t.string    :opening_time
-      t.string    :closing_time
-    end
-  end
-
->>>>>>> 77e4c865504595e4cdfdb18c429bcef7b0232ef8
 end
