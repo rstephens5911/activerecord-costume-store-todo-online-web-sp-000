663f0307be6551db068e5b9414d07f266f9dadb9
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-12 22:55:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,6 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
 
   def change
-<<<<<<< HEAD
     create_table  :haunted_houses do |t|
       t.string    :name
       t.string    :location
@@ -15,18 +14,3 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.timepstamps null: false
     end
   end
-=======
-    create_table :haunted_houses do |t|
-      t.string    :name
-      t.string    :location
-      t.string    :theme
-      t.integer   :price
-      t.boolean   :family_friendly
-      t.string    :opening_date
-      t.string    :closing_date
-      t.string    :description
-    end
-  end
-
->>>>>>> 77e4c865504595e4cdfdb18c429bcef7b0232ef8
-end
